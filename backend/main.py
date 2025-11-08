f70be909642c1007c96ef1bce96084d1e77b12b8
Angela Yu <angelayu0530@gmail.com>
2025-11-08 11:31:38 -0500

    auth working

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,25 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 from sqlalchemy import text
 from db import init_db, db
-from models import *  # Import all models
+from models import *
+from auth.routes import auth_bp
 
 app = Flask(__name__)
+from dotenv import load_dotenv
+import os
+load_dotenv()
+app.config['JWT_SECRET_KEY'] = os.getenv('SECRET_KEY')  
 CORS(app)
 init_db(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
 
-@app.route('/')
-def index():
-    try:
-        result = db.session.execute(text("SELECT NOW()"))
-        current_time = result.fetchone()[0]
-        return jsonify({"message": "Database connected!", "time": str(current_time)})
-    except Exception as e:
-        return jsonify({"error": "Database connection failed", "details": str(e)}), 500
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5001)
